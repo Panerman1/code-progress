4f558366b7416e2e8c5f1428efa04457ef67cbce
Panerman1 <157020659+Panerman1@users.noreply.github.com>
2024-08-10 20:53:04 +0530

    Add files via upload

---
+++ structure.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+struct Cricket
+{
+    char pname[100],tname[100];
+    int bavg;   
+};
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of records:"<<endl;
+    cin>>n;
+    Cricket arr[n];
+    for(int i=0;i<n;i++)
+    {   
+        cout<<"Enter the Player name, Team name and their Batting average of player no. "<<i+1<<":"<<endl;
+        cin>>arr[i].pname;
+        cin>>arr[i].tname;
+        cin>>arr[i].bavg;
+    }
+    for(int j=1;j<n;j++)
+    {
+       
+        for(int i=j;i<n;i++)
+        {
+            int res = strcmp(arr[j-1].tname,arr[j].tname);
+            Cricket temp;
+            if(res>0)
+            {
+                temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    cout<<"The Players and their details arranged in the order of team name are:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout <<arr[i].pname<<", "<<arr[i].tname<<", "<<arr[i].bavg<<endl;
+    }
+    return 0;
+}
